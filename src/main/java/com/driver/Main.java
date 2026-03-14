382ddb9616a3252359dde2939c644613625412cd
Omkar <omkarkumbhar3364@gmail.com>
2026-03-14 13:14:47 +0530

    code updated

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -14,13 +14,9 @@ public class Main {
     }
     public static void main(String[] args) {
         Product p = new Product();
-        int result1=p.product(2,4);
-        System.out.println("Product of 2 integers: " + result1);
 
-        int result2=p.product(2,3,4);
-        System.out.println("Product of 3 integers: " + result2);
-
-        double result3=p.product(2.5,3.5);
-        System.out.println("Product of 2 doubles: " + result3);
+        System.out.println(p.product(2,4));
+        System.out.println(p.product(2,3,4));
+        System.out.println(p.product(2.5,3.5));
     }
 }
